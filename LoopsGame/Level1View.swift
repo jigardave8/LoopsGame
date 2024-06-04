ee73d6349b91f65a80f8f82c3023db4d3ff4e9c1
Jay D <jdavenz8@gmail.com>
2024-06-04 12:10:00 +1200

    main menu, level1 added

---
+++ LoopsGame/Level1View.swift
@@ -0,0 +1,18 @@
+//
+//  Level1View.swift
+//  LoopsGame
+//
+//  Created by Jigar on 04/06/24.
+//
+
+import SwiftUI
+
+struct Level1View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Level1View()
+}
---
+++ LoopsGame/MainMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MainMenuView.swift
+//  LoopsGame
+//
+//  Created by Jigar on 04/06/24.
+//
+
+import SwiftUI
+
+struct MainMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainMenuView()
+}
